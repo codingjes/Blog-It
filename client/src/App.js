f26c0921c7a87a5ffd668f4e14db456cd59414c7
Jesus Maria Casal Torres <codingjes@gmail.com>
2018-12-09 21:57:43 +0100

    created the front end with create react app
    removed the default styles and html, added bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Blog it</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
